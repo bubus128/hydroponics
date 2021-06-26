670298b2863135a346bf4adb5267ba6fdff043dc
Lis <paulinax.lis@intel.com>
2021-06-26 10:20:39 +0200

    Specyfic pin = 17 for dht11

--- dht11.py
+++ dht11.py
@@ -3,8 +3,8 @@ import time
 import adafruit_dht
 import board
 
-# Initial the dht device, with data pin connected to pin???
-dht_device = adafruit_dht.DHT11(board.pin_wich_we_use)
+# Initial the dht device, with data pin connected to pin17
+dht_device = adafruit_dht.DHT11(board.D17)
 humidity = dht_device.humidity
 
 while True:
