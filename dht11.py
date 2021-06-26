9e23b5387a7e3293854e62422de44675f97119c4
Lis <paulinax.lis@intel.com>
2021-06-26 09:57:51 +0200

    Check dht11 temperature measure

---
+++ dht11.py
@@ -0,0 +1,24 @@
+import time
+
+import adafruit_dht
+import board
+
+# Initial the dht device, with data pin connected to pin???
+dht_device = adafruit_dht.DHT11(board.pin_wich_we_use)
+humidity = dht_device.humidity
+
+while True:
+    try:
+        temperature = dht_device.temperature
+        print(f"Temperature: {temperature}")
+
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(1.0)
+        continue
+
+    except Exception as error:
+        dht_device.exit()
+        raise error
+
+    time.sleep(2.0)
