25332d1f14ab14c8ae887e8042ff5b2beaaa6eed
bubus128 <j.swislocki@outlook.com>
2021-07-03 09:26:15 +0200

    atomize done

--- atomize.py
+++ atomize.py
@@ -1,9 +1,24 @@
 import RPi.GPIO as GPIO
 import time
+import sys
+
+'''
+if there is no arguments then switch atomizer on/off
+if there is one argument (delay) then:
+    switch atomizer on
+    wait (delay) seconds
+    switch atomizer off
+'''
 
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(4, GPIO.OUT)
-#GPIO.output(4, GPIO.HIGH) #turn atomizer on
-#time.sleep(100)            #delay 10 seconds
-GPIO.output(4, GPIO.LOW)  #turn atomizer off
+if len(sys.argv)==0:
+    if GPIO.input(4):
+        GPIO.output(4, GPIO.LOW) #turn atomizer on
+    else:
+        GPIO.output(4, GPIO.HIGH)  #turn atomizer off
+else:
+    delay=sys.argv[1]
+    GPIO.output(4, GPIO.HIGH) #turn atomizer on
+    time.sleep(int(delay)) # wait (delay) seconds
+    GPIO.output(4, GPIO.LOW) #turn atomizer back off
