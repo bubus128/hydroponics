7139e9f8bc6f4ac8147d3101d332361a2c2ab8d9
bubus128 <j.swislocki@outlook.com>
2021-07-14 08:50:00 +0200

    sensor switching

--- arduinoSoft/arduinoSoft.ino
+++ arduinoSoft/arduinoSoft.ino
@@ -18,22 +18,26 @@ int pins2[4]={6,7,8,9};
 SyringePump phMinusPump (pins2);
 int pins3[4]={10,11,12,13};
 SyringePump costamPump (pins3);
-int sensor=0;
+string sensor="PH";
 int pomp_buffer[2][30];
 
 void receiveEvent(int byte_count) {
   if(Wire.read()==5){
-    Serial.println("sensor change");
-    sensor=(sensor+1)%2;
-    return;
+    sensor="PH";
   }
-  Wire.read(); //byte_count
-  int pump = Wire.read();
-  int dose = 1000*Wire.read();
-  dosing_queue.add(pump,dose);
+  else if(Wire.read()==6){
+    sensor="TDS"
+  }
+  else{
+    Wire.read(); //byte_count
+    int pump = Wire.read();
+    int dose = 1000*Wire.read();
+    dosing_queue.add(pump,dose);
+  }
+  
 }
 void requestEvent(){
-  if(sensor==0){
+  if(sensor=="TDS"){
     Serial.println("measuring tds");
     int tds=analogRead(TdsSensorPin);
     int compensationCoefficient=1.0+0.02*(temperature-25.0);    
@@ -41,7 +45,7 @@ void requestEvent(){
     tdsValue=(133.42*compensationVolatge*compensationVolatge*compensationVolatge - 255.86*compensationVolatge*compensationVolatge + 857.39*compensationVolatge)*0.5; //convert voltage value to tds value
     Wire.write(tdsValue);
   }
-  else if(sensor==1){
+  else if(sensor=="PH"){
     Serial.println("measuring ph");
     int ph=analogRead(PhSensorPin);
     int phValue=ph*5.0/1024/6*3.5;
@@ -52,7 +56,6 @@ void requestEvent(){
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Wire.begin(0x7);
   Wire.onReceive(receiveEvent);
