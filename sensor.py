58403456b40cdc0186a40f3768c9db7c5ff2aea8
Lis <paulinax.lis@intel.com>
2021-10-21 09:13:13 +0200

    add new classes and major change of MainLoop and Sensor

---
+++ sensor.py
@@ -0,0 +1,44 @@
+from Logger import Logger
+import adafruit_dht
+import board
+
+
+class Sensor:
+    def __init__(self):
+        self.modules = {
+            'temperature': True,
+            'humidity': True,
+            'PH': True,
+            'TDS': True,
+            'water_level': False,
+            'lights': True,
+            'tsl': True
+        }
+        self.sensors_indications = {  # should be in json or separate file
+            'ph': None,
+            'tds': None,
+            'light': None,
+            'temperature': None,
+            'humidity': None
+        }
+
+        self.gpi_pins_dict = {
+            'atomizer': 4,
+            'cooling': 14,
+            'fan': 15
+        }
+        self.logger = Logger()
+        self.dht_devices = [adafruit_dht.DHT11(board.D17), adafruit_dht.DHT11(board.D27)]
+
+    def on_off(self, **kwargs):
+        pass
+
+    def read_value(self, **kwargs):
+        pass
+
+
+
+
+
+
+
