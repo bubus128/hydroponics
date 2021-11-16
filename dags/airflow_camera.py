bf9229630dec92d4bd4162c3fad1af7bf0f34f63
Jakub Zwolski <44641091+zwojak@users.noreply.github.com>
2021-11-17 00:49:14 +0100

    first version of the rest of dags

--- dags/airflow_camera.py
+++ dags/airflow_camera.py
@@ -4,62 +4,29 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.python import BranchPythonOperator
+from airflow.models import Variable
 from airflow.utils.dates import days_ago
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 import requests
 from time import sleep
-from airflow.models import Variable
+import glob
+import json
 
-address = ('https://1027f0e835e5.ngrok.io/')
-amount_of_checks = 3
-pH_upper_limit = 27
-pH_lower_limit = 21
-pH_middle_value = 24
+address = Variable.get("RPI_IP", deserialize_json=False)
 
 args = {
     'owner': 'airflow',
 }
 
 def take_photo():
-    r = requests.get(address + 'take_photo')
-
-
-def recognize_phase():
-    if Variable.get("WHAT_PHASE_IT_IT") == "growth":
-        #TODO recognizing phase
-        if(True):
-            Variable.set("WHAT_PHASE_IT_IT", "flowering")
-
-def send_photo():
-    print("SEND_PHOTO")
-
-def recognize_disease():
-    #TODO recognize disease
-    if(False):
-        return 'plague'
-    elif(False):
-        return 'lack_of_minerals'
-    elif(False):
-        return 'sick'
-    return 'all_is_ok'
-
-def sick():
-    print("PLANTS SICK!")
-
-def lack_of_minerals():
-    print("PLANTS LACKING MINERALS!")
-
-def plague():
-    print("THERES PLAGUE!")
-
-
-
+    r = requests.get(address + '/takePhoto')
+    return
 
 with DAG(
     dag_id='airflow_camera',
     default_args=args,
     catchup=False,
-    schedule_interval='0 */8 * * *', #TODO CHANGE TO TRIGGER WHEN TAKS IN 'LIGHT' SCRIPT DONE
+    schedule_interval='0 */1 * * *',
 	max_active_runs=1,
     start_date=days_ago(2),
     dagrun_timeout=timedelta(hours=10),
@@ -70,45 +37,6 @@ with DAG(
         task_id='take_photo',
         python_callable=take_photo
     )
-
-    recognize_phase = PythonOperator(
-        task_id='recognize_phase',
-        python_callable=recognize_phase
-    )
-
-    send_photo = PythonOperator(
-        task_id='send_photo',
-        python_callable=send_photo
-    )
-
-    recognize_disease = BranchPythonOperator(
-        task_id='recognize_disease',
-        python_callable=recognize_disease
-    )
-
-    all_is_ok = DummyOperator(
-        task_id='all_is_ok'
-    )
-
-    plague = PythonOperator(
-        task_id='plague',
-        python_callable=plague
-    )
-
-    lack_of_minerals = PythonOperator(
-        task_id='lack_of_minerals',
-        python_callable=lack_of_minerals
-    )
-
-    sick = PythonOperator(
-        task_id='sick',
-        python_callable=sick
-    )
-
-take_photo >> recognize_phase >> send_photo >> recognize_disease >> all_is_ok
-take_photo >> recognize_phase >> send_photo >> recognize_disease >> plague
-take_photo >> recognize_phase >> send_photo >> recognize_disease >> lack_of_minerals
-take_photo >> recognize_phase >> send_photo >> recognize_disease >> sick
 
 if __name__ == "__main__":
     dag.cli()
---
+++ dags/airflow_tds.py
@@ -0,0 +1,86 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+from airflow.operators.python import BranchPythonOperator
+from airflow.models import Variable
+from airflow.utils.dates import days_ago
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+import requests
+from time import sleep
+import glob
+import json
+
+address = Variable.get("RPI_IP", deserialize_json=False)
+variables = Variable.get("indication_limits", deserialize_json=True)
+ideal_tds = variables[Variable.get("phase")]['tds']["standard"]
+hysteresis = variables[Variable.get("phase")]['tds']["hysteresis"]
+fertilizer_dosing = False
+
+
+args = {
+    'owner': 'airflow',
+}
+
+def measure_tds():
+    r = requests.get(address + '/get/tds')
+    return float(r.text)
+
+def check_tds():
+    tds = measure_tds()
+    tds_limit = ideal_tds if fertilizer_dosing else ideal_tds - hysteresis
+    if tds > ideal_tds + hysteresis:
+        return 'lower_tds'
+    elif tds < tds_limit:
+        return 'raise_tds'
+    return 'everything_is_ok'
+	
+def raise_the_tds():
+    r = requests.get(address + '/dose/fertilizer')
+    fertilizer_dosing = True
+    return
+
+def lower_the_tds():
+    return
+
+def ok_tds():
+    fertilizer_dosing = False
+    return
+	
+with DAG(
+    dag_id='airflow_tds',
+    default_args=args,
+    catchup=False,
+    schedule_interval='*/1 * * * *',
+	max_active_runs=1,
+    start_date=days_ago(2),
+    dagrun_timeout=timedelta(hours=10),
+    params={"example_key": "example_value"},
+) as dag:
+
+    check_tds = BranchPythonOperator(
+        task_id='check_tds',
+        python_callable=check_tds
+    )
+
+    lower_tds = PythonOperator(
+        task_id='lower_tds',
+        python_callable=lower_the_tds
+    )
+    raise_tds = PythonOperator(
+        task_id='raise_tds',
+        python_callable=raise_the_tds
+    )
+	
+    everything_is_ok = PythonOperator(
+        task_id='everything_is_ok',
+        python_callable=ok_tds
+    )
+	
+check_tds >> lower_tds
+check_tds >> raise_tds
+check_tds >> everything_is_ok
+
+if __name__ == "__main__":
+    dag.cli()
